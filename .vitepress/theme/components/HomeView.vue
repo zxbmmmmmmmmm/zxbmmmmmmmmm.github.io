04cb432f5711ebcce07a72a0541f52809458cf66
Betta_Fish <zxbmmmmmmmmm@qq.com>
2026-06-18 13:10:55 +0800

    add tiles

--- .vitepress/theme/components/HomeView.vue
+++ .vitepress/theme/components/HomeView.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
 import ArticleList from './ArticleList.vue'
+import Tile from './Tile.vue'
 import { data as posts } from '../posts.data'
 </script>
 
 <template>
   <div class="layout">
+    <div class="home-view-header">
+      <Tile theme="accent"> 11 </Tile>
+      <Tile theme="accent" size="wide"> 11 </Tile>
+    </div>
     <ArticleList :posts="posts" />
   </div>
 </template>
 <style scoped>
-.layout{
-  padding: 1rem
+.layout {
+  padding: 1rem;
+  display: grid;
+  gap: 1rem;
+}
+.home-view-header {
+  display: flex;
+  gap: 1rem;
 }
 </style>
---
+++ .vitepress/theme/components/Tile.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import VButton from './VButton.vue'
+interface Props {
+  text?: string
+  theme?: 'default' | 'accent'
+  size?: 'small' | 'medium' | 'wide' | 'large'
+}
+const props = withDefaults(defineProps<Props>(), {
+  text: 'Text',
+  theme: 'default',
+  size: 'medium'
+})
+</script>
+<template>
+  <VButton type="button" class="tile-button" :class="[size]" :theme="theme">
+    <slot class="tile-content">11</slot>
+    <p class="tile-text">{{ text }}</p>
+  </VButton>
+</template>
+<style scoped>
+.tile-button {
+  font-size: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center; /* 垂直居中 */
+  align-items: center; /* 水平居中 */
+  position: relative;
+}
+.tile-content {
+  grid-area: 1 / 1 / 2 / 2;
+  align-self: center;
+  justify-self: center;
+}
+.tile-text {
+  font-size: 0.8rem;
+  position: absolute;
+  right: 0.4rem;
+  bottom: 0.2rem;
+}
+
+.tile-button.small {
+  width: 50px;
+  height: 50px;
+  .tile-text {
+    display: none;
+  }
+}
+.tile-button.medium {
+  width: 100px;
+  height: 100px;
+}
+.tile-button.wide {
+  width: 200px;
+  height: 100px;
+}
+.tile-button.large {
+  width: 200px;
+  height: 200px;
+}
+</style>
